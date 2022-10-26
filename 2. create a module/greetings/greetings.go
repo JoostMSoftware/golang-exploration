17db32002f12e7ddc71ce8b0f8d1246ad7ef94a1
JoostMSoftware <joostmsoftware@gmail.com>
2022-10-26 14:50:17 +0200

    Random greetings with error handling

--- 2. create a module/greetings/greetings.go
+++ 2. create a module/greetings/greetings.go
@@ -1,8 +1,44 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hey, %v. Welcome to this tutorial!", name)
-	return message
+// Hello returns a greeting for the named person
+func Hello(name string) (string, error) {
+	// If there was no name given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// init sets the initial values for the variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+/*
+randomFormat returns one of a set of greeting messages. The returned
+message is selected at random.
+*/
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v.",
+		"Hello, %v! Well met!",
+	}
+
+	/*
+		Return a randomly selected message format by specifying
+		a random index for the slice of formats.
+	*/
+	return formats[rand.Intn(len(formats))]
 }
--- 2. create a module/hello/hello.go
+++ 2. create a module/hello/hello.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"joostmsoftware.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Joost")
-	fmt.Println(message)
+	/*
+		Set properties of the predefined Loggers, including
+		the log entry prefix and add a flag to disable printing
+		the time, source file and line number.
+	*/
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
+	// Request a greeting message.
+	message, err := greetings.Hello("Test")
+
+	/*
+		If an error was returned, print it to the console and
+		exit the program.
+	*/
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		If no error was returned, print the returned message
+		to the console
+	*/
+	fmt.Println(message)
 }
